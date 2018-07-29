1148d598766cef134df9fa0f127e2c6c67f12c42
Thomas Jäckle <thjaeckle@gmail.com>
2018-07-29 08:58:03 +0200

    fixed sorting album contents by image name

--- site-builder/index.js
+++ site-builder/index.js
@@ -41,7 +41,11 @@ function folderName(path) {
 
 function getAlbums(data) {
   var objects = data.Contents.sort(function(a,b){
-    return a.Key - b.Key;
+    var x = a.Key.toLowerCase();
+    var y = b.Key.toLowerCase();
+    if (x < y) return 1;
+    if (x > y) return -1;
+    return 0;
   }).map(stripPrefix);
   var albums = objects.map(folderName);
   // Deduplicate albums
@@ -75,10 +79,10 @@ function uploadHomepageSite(albums, pictures, metadata) {
           if (metadata[i] && metadata[i].title) {
             albumTitle = metadata[i].title;
           }
-          picturesHTML += "\t\t\t\t\t\t<article class=\"thumb\">\n" +
-                          "\t\t\t\t\t\t\t<a href=\"" + albums[i] + "/index.html\" class=\"image\"><img src=\"/pics/resized/1200x750/" + pictures[i][0] + "\" alt=\"\" /></a>\n" +
+          picturesHTML = "\t\t\t\t\t\t<article class=\"thumb\">\n" +
+                          "\t\t\t\t\t\t\t<a href=\"" + albums[i] + "/index.html\" class=\"image\"><img src=\"/pics/resized/1200x750/" + pictures[i][pictures[i].length-1] + "\" alt=\"\" /></a>\n" +
                           "\t\t\t\t\t\t\t<h2>" + albumTitle + "</h2>\n" +
-                          "\t\t\t\t\t\t</article>\n";
+                          "\t\t\t\t\t\t</article>\n" + picturesHTML;
         }
         body = body.toString().replace(/\{title\}/g, process.env.WEBSITE_TITLE)
                               .replace(/\{pictures\}/g, picturesHTML);
